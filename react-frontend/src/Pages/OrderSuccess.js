da85312235983bd58afab6a0fb8b5867af0f9a73
Christos Sampakidis <csam@iphost.net>
2024-04-09 10:15:16 +0300

    complete changes

--- react-frontend/src/Pages/OrderSuccess.js
+++ react-frontend/src/Pages/OrderSuccess.js
@@ -2,8 +2,18 @@ import React, { useState, useEffect } from 'react';
 import {Link, useLocation, useParams} from 'react-router-dom';
 
 const OrderSuccess = () => {
-    return (
-        <div style={{ height: "100%", width: "100%", color: "black", fontSize:"5em",textAlign:"center",fontFamily:"sans-serif"}}>THANK YOU FOR YOUR ORDER</div>
+    return (<>
+        <div style={{
+            height: "100%",
+            width: "100%",
+            color: "black",
+            fontSize: "5em",
+            textAlign: "center",
+            fontFamily: "sans-serif",
+            marginTop: "5%"
+        }}>THANK YOU FOR YOUR ORDER</div>
+            <i className="text-center w-100 fa-solid fa-check fa-10x" style={{padding: "5%", color: "#39977E"}}></i>
+        </>
     );
 };
 
