398867e7fc4e00c098b5a47a38cf4664c4f20a0c
Christos Sampakidis <csam@iphost.net>
2024-04-15 15:04:30 +0300

    complete changes

--- react-frontend/src/component/Logout.js
+++ react-frontend/src/component/Logout.js
@@ -1,24 +1,27 @@
 import React, { useState } from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import './NavBar.css';
+import $ from "jquery";
 
 
 const Logout = () => {
-    function myprofileinfo(){
-        window.location.href = "/MyProfileInfo";
-    }
+
     const navigate = useNavigate();
 
     function logout(){
         if(sessionStorage.getItem('token')){
+            $('#regpopup').hide();
             sessionStorage.removeItem("token");
             navigate('/');
             window.location.reload();
         }
     }
+    function togglepopup() {
+            $('#regpopup').hide();
+    }
     return (
         <div id="regpopup">
-            <Link className="login-button" to="/MyProfileInfo">Profile</Link>
+            <Link onClick={togglepopup} className="profile-button" to="/MyProfileInfo">Profile</Link>
             <button className="logout-button" onClick={logout}> Logout </button>
         </div>
 
