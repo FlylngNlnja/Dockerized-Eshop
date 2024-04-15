36d6c0be78bcb1c4b1b908defa439bbb86e1e270
Christos Sampakidis <csam@iphost.net>
2024-04-15 14:31:04 +0300

    complete changes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM openjdk:21-jdk AS build
 WORKDIR /app
-RUN mvn clean package -DskipTests
 COPY target/demo-0.0.1-SNAPSHOT.jar app.jar
 EXPOSE 8080
 
--- react-frontend/src/component/Footer.js
+++ react-frontend/src/component/Footer.js
@@ -6,20 +6,23 @@ const Footer = () => {
     return (
        <>
            <div className="Footer">
-               <div className="Credits">
-                   Developed by <br/>
-                   <a className="text-decoration-none" href="https://github.com/FlylngNlnja" target="_blank"
-                      rel="noreferrer"> Christos S.</a> <i className="fa-solid fa-plus"></i>
-                   <a className="text-decoration-none" href="https://github.com/Gsotis" target="_blank"
-                      rel="noreferrer"> Gsotis</a> <i className="fa-solid fa-plus"></i>
-                   <a className="text-decoration-none" href="https://github.com/ChrisMi1" target="_blank"
-                      rel="noreferrer"> Christos M.</a>
-               </div>
                <div className="Frameworks">
 
-                   <a  target="_blank" href="https://react.dev/"><i className="fa-brands fa-react fa-9x" style={{color: "#3ab7ff"}}> </i></a>
+                   <a target="_blank" href="https://react.dev/"><i className="fa-brands fa-react fa-9x"
+                                                                   style={{color: "#3ab7ff"}}> </i></a>
 
                </div>
+               <div className="Credits">
+                   <a className="text-decoration-none" href="https://github.com/ChrisMi1" target="_blank"
+                      rel="noreferrer"> Christos M.</a><br/>Fullstack<br/><br/>
+                   <a className="text-decoration-none" href="https://github.com/FlylngNlnja" target="_blank"
+                      rel="noreferrer"> Christos S.</a> <br/>Fullstack<br/><br/>
+                   <a className="text-decoration-none" href="https://github.com/Gsotis" target="_blank"
+                      rel="noreferrer"> Gsotis</a> <br/>Frontend<br/><br/>
+                   <a className="text-decoration-none" href="https://github.com/ghintipas0" target="_blank"
+                      rel="noreferrer"> Ghintipas</a><br/>Frontend
+               </div>
+
            </div>
 
        </>
